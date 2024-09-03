2d265824804baa4b08b4cdb041c23ab6e47ab2ba
anacarolinebaldessar <125361928+ViniBaiak6510@users.noreply.github.com>
2024-09-03 20:10:27 -0300

    Menu

---
+++ Main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(){
+   int sair = 1, escolha = 0;
+
+   do{
+        printf("Escolha uma das opcoes a seguir:\n1.Saque\n2.Deposito\n3.Emprestimo\n4.Consulta de Saldo\n5.Extrato\n6.Transferencia\n7.Sair\n");
+        scanf("%d", &escolha);
+
+        if(escolha == 1){
+
+        }
+        else if(escolha == 2){
+
+        }
+        else if(escolha == 3){
+
+        }
+        else if(escolha == 4){
+
+        }
+        else if(escolha == 5){
+
+        }
+        else if(escolha == 6){
+
+        }
+        else{
+            sair = 0;
+        }
+   }while(sair != 0);
+}
